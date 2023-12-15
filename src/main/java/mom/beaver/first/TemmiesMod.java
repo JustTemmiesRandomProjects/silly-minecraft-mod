d4e8b517646ed9502f9fc9c57610d5a484cd7e60
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2023-12-15 13:00:56 +0100

    stuff

--- src/main/java/mom/beaver/first/TemmiesMod.java
+++ src/main/java/mom/beaver/first/TemmiesMod.java
@@ -2,9 +2,6 @@ package mom.beaver.first;
 
 import mom.beaver.first.features.dam.DamFeature;
 import mom.beaver.first.features.dam.DamFeatureConfig;
-import mom.beaver.first.features.example.ExampleFeature;
-import mom.beaver.first.features.example.ExampleFeatureConfig;
-import mom.beaver.first.registry.RegisterItems;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
@@ -22,15 +19,8 @@ import org.slf4j.LoggerFactory;
 
 public class TemmiesMod implements ModInitializer {
 	public static String MOD_ID = "temmies-mod";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
-	public static final Identifier EXAMPLE_FEATURE_ID = new Identifier("temmies-mod", "example_feature");
-	public static Feature<ExampleFeatureConfig> EXAMPLE_FEATURE = new ExampleFeature(ExampleFeatureConfig.CODEC);
 
 	public static final Identifier DAM_FEATURE_ID = new Identifier("temmies-mod", "dam");
 	public static Feature<DamFeatureConfig> DAM_FEATURE = new DamFeature(DamFeatureConfig.CODEC);
@@ -40,15 +30,8 @@ public class TemmiesMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Loading Temmie's Custom-Structures");
 
-		LOGGER.info("Hello Fabric world!");
-
-		RegisterItems.register();
-
-		Registry.register(Registries.FEATURE, EXAMPLE_FEATURE_ID, EXAMPLE_FEATURE);
 		Registry.register(Registries.FEATURE, DAM_FEATURE_ID, DAM_FEATURE);
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.LOCAL_MODIFICATIONS, CUSTOM_DAM_PLACED_KEY);
 	}
